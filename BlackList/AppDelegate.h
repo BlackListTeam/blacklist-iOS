ab9c54c571b37d919482c2cd2f97d6af841335bc
albertesga <albertesga@gmail.com>
2013-06-20 18:28:09 +0200

    Initial Commit

---
+++ BlackList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlackList
+//
+//  Created by Albert on 20/06/13.
+//  Copyright (c) 2013 AndreuRM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlackList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlackList
+//
+//  Created by Albert on 20/06/13.
+//  Copyright (c) 2013 AndreuRM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
