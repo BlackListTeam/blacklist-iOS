4609d6537036faf67d88e80b99479458a6416105
andreurm <andreurm@gmail.com>
2013-07-01 17:14:41 +0200

    JSon lib

---
+++ BlackList/promoterCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  promoterCodeViewController.h
+//  BlackList
+//
+//  Created by Air on 01/07/13.
+//  Copyright (c) 2013 AndreuRM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface promoterCodeViewController : UIViewController
+
+@end
---
+++ BlackList/promoterCodeViewController.m
@@ -0,0 +1,38 @@
+//
+//  promoterCodeViewController.m
+//  BlackList
+//
+//  Created by Air on 01/07/13.
+//  Copyright (c) 2013 AndreuRM. All rights reserved.
+//
+
+#import "promoterCodeViewController.h"
+
+@interface promoterCodeViewController ()
+
+@end
+
+@implementation promoterCodeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
