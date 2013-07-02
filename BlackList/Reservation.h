0281bb3d7b22cfc0a174761baf8e9284903f7cfc
andreurm <andreurm@gmail.com>
2013-07-02 17:12:39 +0200

    asd

---
+++ BlackList/Reservation.h
@@ -0,0 +1,13 @@
+//
+//  Reservation.h
+//  WsTest
+//
+//  Created by Air on 28/06/13.
+//  Copyright (c) 2013 Air. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Reservation : NSObject
+
+@end
---
+++ BlackList/Reservation.m
@@ -0,0 +1,13 @@
+//
+//  Reservation.m
+//  WsTest
+//
+//  Created by Air on 28/06/13.
+//  Copyright (c) 2013 Air. All rights reserved.
+//
+
+#import "Reservation.h"
+
+@implementation Reservation
+
+@end
