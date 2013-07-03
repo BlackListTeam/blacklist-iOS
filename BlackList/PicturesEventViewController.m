ab2871c9051a14ced0a7458747fcf5a9e87b98a5
albertesga <albertesga@gmail.com>
2013-07-03 17:00:48 +0200

    PROBLEMS AMB LES IMATGES DINAMIQUES

--- BlackList/PicturesEventViewController.m
+++ BlackList/PicturesEventViewController.m
@@ -7,10 +7,11 @@
 //
 
 #import "PicturesEventViewController.h"
+#import "AsyncImageView.h"
 
 @interface PicturesEventViewController ()
 
-@property (nonatomic, retain) NSMutableArray *items;
+@property (nonatomic, retain) NSMutableArray *imageURLs;
 
 @end
 
@@ -18,7 +19,7 @@
 
 
 @synthesize carousel;
-@synthesize items;
+@synthesize imageURLs;
 
 - (void)awakeFromNib
 {
@@ -26,12 +27,27 @@
     //your carousel should always be driven by an array of
     //data of some kind - don't store data in your item views
     //or the recycling mechanism will destroy your data once
-    //your item views move off-screen
-    self.items = [NSMutableArray array];
-    for (int i = 0; i < 1000; i++)
+	
+	//get image URLs
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"Images" ofType:@"plist"];
+    NSArray *imagePaths = [NSArray arrayWithContentsOfFile:plistPath];
+    
+    //remote image URLs
+    NSMutableArray *URLs = [NSMutableArray array];
+    for (NSString *path in imagePaths)
     {
-        [items addObject:[NSNumber numberWithInt:i]];
+        NSURL *URL = [NSURL URLWithString:path];
+        if (URL)
+        {
+            [URLs addObject:URL];
+        }
+        else
+        {
+            NSLog(@"'%@' is not a valid URL", path);
+        }
     }
+    self.imageURLs = URLs;
+
 }
 
 - (void)dealloc
@@ -73,7 +89,7 @@
 - (NSUInteger)numberOfItemsInCarousel:(iCarousel *)carousel
 {
     //return the total number of items in the carousel
-    return [items count];
+    return [imageURLs count];
 }
 
 - (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSUInteger)index reusingView:(UIView *)view
@@ -83,7 +99,19 @@
     //create new view if no view is available for recycling
     if (view == nil)
     {
-        view = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200.0f, 200.0f)];
+        
+        AsyncImageView *view = [[AsyncImageView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 200.0f, 200.0f)];
+        view.imageURL = [imageURLs objectAtIndex:index];
+		view.contentMode = UIViewContentModeCenter;
+        label = [[UILabel alloc] initWithFrame:view.bounds];
+        label.backgroundColor = [UIColor clearColor];
+        label.textAlignment = UITextAlignmentCenter;
+        label.font = [label.font fontWithSize:50];
+        label.tag = 1;
+        [view addSubview:label];
+
+        
+        /*view = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200.0f, 200.0f)];
         ((UIImageView *)view).image = [UIImage imageNamed:@"page.png"];
         view.contentMode = UIViewContentModeCenter;
         label = [[UILabel alloc] initWithFrame:view.bounds];
@@ -91,7 +119,7 @@
         label.textAlignment = UITextAlignmentCenter;
         label.font = [label.font fontWithSize:50];
         label.tag = 1;
-        [view addSubview:label];
+        [view addSubview:label];*/
     }
     else
     {
@@ -104,7 +132,7 @@
     //views outside of the `if (view == nil) {...}` check otherwise
     //you'll get weird issues with carousel item content appearing
     //in the wrong place in the carousel
-    label.text = [[items objectAtIndex:index] stringValue];
+    label.text = [[imageURLs objectAtIndex:index] stringValue];
     
     return view;
 }
