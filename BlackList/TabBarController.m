9463b74473672deb2f85a38929d6ab85f15e4043
albertesga <albertesga@gmail.com>
2013-07-22 20:36:29 +0200

    tabbar

--- BlackList/TabBarController.m
+++ BlackList/TabBarController.m
@@ -27,7 +27,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    [self.tabBar setBackgroundColor:[[UIColor alloc] initWithRed:0 green:0 blue:0 alpha:1]];
+    /*self.tabBar.tintColor = [UIColor clearColor];
+    self.tabBar.shadowImage = nil;*/
+    
     UITabBarItem *tabBarItem = [[self.tabBar items] objectAtIndex:0];
     [tabBarItem setFinishedSelectedImage:[UIImage imageNamed:@"0.homeS.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"0.home.png"]];
     UITabBarItem *tabBarItem1 = [[self.tabBar items] objectAtIndex:1];
@@ -36,7 +38,10 @@
     [tabBarItem2 setFinishedSelectedImage:[UIImage imageNamed:@"0.codeS.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"0.code.png"]];
     UITabBarItem *tabBarItem3 = [[self.tabBar items] objectAtIndex:3];
     [tabBarItem3 setFinishedSelectedImage:[UIImage imageNamed:@"0.inviteS.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"0.invite.png"]];
-    //self.tabBar.tintColor = [UIColor blueColor];
+    
+    self.tabBar.backgroundImage = [UIImage imageNamed:@"menu"];
+    self.tabBar.selectedImageTintColor = nil;
+    self.tabBar.selectionIndicatorImage = [[UIImage alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
